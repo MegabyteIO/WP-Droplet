1a00a0bfcfe795449043e7542b599899647ece0f
MegabyteIO <brian@envied.solutions.com>
2013-11-10 01:41:04 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /usr/local/src
+# Updates all software except the kernel
+yum -y --exclude=kernel* update
+# Installs some dependencies
+yum -y install git bc
+git clone https://github.com/MegabyteIO/WP-Droplet.git MegabyteIO
+cd MegabyteIO
+chmod +x setup.sh
+./setup.sh
