82c2859fa5bc34975a934546c1e0c2592beadd9a
MegabyteIO <brian@envied.solutions.com>
2013-11-10 02:30:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 yum -y --exclude=kernel* update
 # Installs some dependencies
 yum -y install git bc
-cd /usr/local/src
-git clone https://github.com/MegabyteIO/WP-Droplet.git MegabyteIO && cd MegabyteIO && cd scripts
+git clone https://github.com/MegabyteIO/WP-Droplet.git /usr/local/src/megabyteio
+cd /usr/local/src/megabyteio/scripts
 chmod +x setup.sh
 ./setup.sh
